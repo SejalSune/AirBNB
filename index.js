efa580adebf4000f33e0f57127fd9c69a6b3dbd0
sejal sune <sunesejal@gmail.com>
2024-05-11 11:04:11 +0530

    photos on cloud

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const multer  = require('multer');
+const {storage}=require("./cloudconfig");
+const upload = multer({ storage });
 const express=require("express");
 engine = require('ejs-mate');
 const app=express();
@@ -6,6 +10,8 @@ const ejs=require("ejs");
 const listing = require("./models/listing");
 const path=require("path");
 const methodOverride = require('method-override');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs'); 
@@ -13,6 +19,18 @@ app.use(methodOverride('_method'));
 app.use(express.static('views'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(flash());  // flash messages
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use((req,res,next)=>{
+    res.locals.msg=req.flash("success");
+    next();
+});
 
 // alllisting route 
 app.get("/listings",async (req,res)=>{
@@ -36,20 +54,25 @@ app.get("/listings/:id/edit",async (req,res) => {
 app.put("/listings/:id",async (req,res)=> {
     let {id}=req.params;
     await listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash('success', 'Airbnb updated successfully');
     res.redirect(`/listings/${id}`);
 });
 
 app.delete("/listings/:id",async (req,res) => {
     let {id}=req.params;
     let deletedlist=await listing.findByIdAndDelete(id);
+    req.flash('success', 'Airbnb removed successfully');
     res.redirect(`/listings`);
     console.log(deletedlist);
 });
 
 // new data add 
-app.post("/listings",async (req,res) => {
+app.post("/listings",upload.single('listing[photo]'), async (req, res, next) => {
+    let URL=req.file.path;
     let newlisting=await new listing(req.body.listing);
+    newlisting.photo=URL;
     await newlisting.save();
+    req.flash('success', 'Airbnb created successfully');
     res.redirect("/listings");
 });
 
@@ -58,6 +81,11 @@ app.get("/listings/:id",async (req,res)=>{
     let {id}=req.params;
     let listings=await listing.findById(id);
     res.render("detail.ejs",{listings});
+});
+
+// details route
+app.use(async (req,res)=>{
+    res.render("error.ejs");
 });
 
 app.listen(8080,()=>{
--- models/listing.js
+++ models/listing.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 const listingschema = new mongoose.Schema({
     title: String,
-    photo: String,
+    photo: Array,
     description:String,
     price:Number,
     location:String,
